f7da09daac4c27a4542dc506c4a0a4b71d16754c
yogingale <yogeshingale94@gmail.com>
2019-11-16 18:43:04 +0530

    Fixing Travis build

--- app.py
+++ app.py
@@ -4,13 +4,11 @@ import os
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 
+
 @app.route('/')
 def hello_world():
     return render_template('index.html')
 
-@app.route('/<name>')
-def hello_name(name):
-    return "Hello {}!".format(name)
 
 if __name__ == '__main__':
     app.run()
