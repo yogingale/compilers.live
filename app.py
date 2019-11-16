e7b941e79eca6c4dba742b4f2f3cb392ddb60f1a
yogingale <yogeshingale94@gmail.com>
2019-11-16 18:08:33 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+import os
+
+app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+
+@app.route('/<name>')
+def hello_name(name):
+    return "Hello {}!".format(name)
+
+if __name__ == '__main__':
+    app.run()
